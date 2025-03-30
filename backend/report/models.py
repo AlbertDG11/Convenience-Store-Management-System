211ac50351d299c7fd016d298381e14c4506d36a
Jiaqi <jiaqi.chen1@ucalgary.ca>
2025-03-30 14:28:13 -0600

    models of product, report and supplier

--- backend/report/models.py
+++ backend/report/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+<<<<<<< HEAD
+=======
+class Report(models.Model):
+    daily_report = models.CharField(max_length=50)
+>>>>>>> Jiaqi_Chen
