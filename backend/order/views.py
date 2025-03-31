b40bef14cfa0cf6a0a58d7b86865bb2399af0b4d
Zhuojun Dong <albertdong11@gmail.com>
2025-03-30 22:27:45 -0600

    add cancel_order method

--- backend/order/views.py
+++ backend/order/views.py
@@ -1,6 +1,9 @@
+from django.db import transaction
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from backend.employee.permissions import IsSalesPersonOrManager
 from backend.order.models import Order
@@ -30,6 +33,31 @@ class OrderViewSet(viewsets.ModelViewSet):
         if membership_id:
             queryset = queryset.filter(membership_id=membership_id)
         return queryset
+
+    @action(detail=True, methods=['post'], url_path = 'cancel')
+    def cancel_order(self, request):
+        """
+        Cancel an order, sets the status to Canceled and restocks inventory
+        """
+        order = self.get_object()
+
+        if order.status == 'Canceled':
+            return Response({'detail: Order has been canceled'}, status=status.HTTP_400_BAD_REQUEST)
+
+        with transaction.atomic():
+            for item in order.items.all():
+                product = item.product
+                product.stock += item.quantity
+                product.save()
+
+            order.status = 'Canceled'
+            order.save()
+
+        # Return updated data
+        serializer = self.get_serializer(order)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
 
 
 
