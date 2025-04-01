307d7e8a1b3001c2f8b7eb2209ff685498108f3e
Fei Ding <dingfei.sgad@gmail.com>
2025-04-01 02:27:53 -0600

    Adjust Serialisers and update Employee query function

--- backend/employee/serializer.py
+++ backend/employee/serializer.py
@@ -1,12 +1,13 @@
 from rest_framework import serializers
 from .models import *
 
-# Serialiser for Employee model
-class EmployeeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Employee
-        fields = ['employee_id', 'name', 'email', 'phone_number', 
-                  'salary', 'login_password']
+
+# # Serialiser for Employee model
+# class EmployeeSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Employee
+#         fields = ['employee_id', 'name', 'email', 'phone_number', 
+#                   'salary', 'login_password']
 
 
 # Serialiser for Employee Address model
@@ -18,48 +19,64 @@ class EmployeeAddressSerializer(serializers.ModelSerializer):
         fields = ['employee_id', 'province', 'city', 'street_address', 'post_code']
 
 
-# Serialiser for Salesperson model
-class SalespersonSerializer(serializers.ModelSerializer):
-    employee = EmployeeSerializer()
-
-    class Meta:
-        model = Salesperson
-        fields = ['employee', 'sales_target']
-
-
-# Serialiser for Purchase Person model
-class PurchasePersonSerializer(serializers.ModelSerializer):
-    employee = EmployeeSerializer()
-
-    class Meta:
-        model = PurchasePerson
-        fields = ['employee', 'purchase_section']
+# Serialiser for Whole Employee model
+class WholeEmployeeSerializer(serializers.Serializer):
+    employee_id = serializers.IntegerField()
+    name = serializers.CharField()
+    email = serializers.CharField()
+    phone_number = serializers.CharField()
+    salary = serializers.FloatField()
+    login_password = serializers.CharField()
+    addresses = EmployeeAddressSerializer(many=True)
+    role = serializers.IntegerField()
+    sales_target = serializers.FloatField(required=False, allow_null=True)
+    purchase_section = serializers.CharField(required=False, allow_null=True)
+    management_level = serializers.CharField(required=False, allow_null=True)
+    management = serializers.ListField(child = serializers.IntegerField)
 
 
-# Serialiser for Manager model
-class ManagerSerializer(serializers.ModelSerializer):
-    employee = EmployeeSerializer()
+# # Serialiser for Salesperson model
+# class SalespersonSerializer(serializers.ModelSerializer):
+#     employee = EmployeeSerializer()
 
-    class Meta:
-        model = Manager
-        fields = ['employee', 'management_level']
-
-
-# Serialiser for Salesperson Manager Management model
-class SalespersonManagerManagementSerializer(serializers.ModelSerializer):
-    salesperson = SalespersonSerializer()
-    manager = ManagerSerializer()
-
-    class Meta:
-        model = SalespersonManagerManagement
-        fields = ['salesperson', 'manager']
+#     class Meta:
+#         model = Salesperson
+#         fields = ['employee', 'sales_target']
 
 
-# Serialiser for Purchaseperson Manager Management model
-class PurchasepersonManagerManagementSerializer(serializers.ModelSerializer):
-    purchaseperson = PurchasePersonSerializer()
-    manager = ManagerSerializer()
+# # Serialiser for Purchase Person model
+# class PurchasePersonSerializer(serializers.ModelSerializer):
+#     employee = EmployeeSerializer()
 
-    class Meta:
-        model = PurchasepersonManagerManagement
-        fields = ['purchaseperson', 'manager']
+#     class Meta:
+#         model = PurchasePerson
+#         fields = ['employee', 'purchase_section']
+
+
+# # Serialiser for Manager model
+# class ManagerSerializer(serializers.ModelSerializer):
+#     employee = EmployeeSerializer()
+
+#     class Meta:
+#         model = Manager
+#         fields = ['employee', 'management_level']
+
+
+# # Serialiser for Salesperson Manager Management model
+# class SalespersonManagerManagementSerializer(serializers.ModelSerializer):
+#     salesperson = SalespersonSerializer()
+#     manager = ManagerSerializer()
+
+#     class Meta:
+#         model = SalespersonManagerManagement
+#         fields = ['salesperson', 'manager']
+
+
+# # Serialiser for Purchaseperson Manager Management model
+# class PurchasepersonManagerManagementSerializer(serializers.ModelSerializer):
+#     purchaseperson = PurchasePersonSerializer()
+#     manager = ManagerSerializer()
+
+#     class Meta:
+#         model = PurchasepersonManagerManagement
+#         fields = ['purchaseperson', 'manager']
--- backend/employee/views.py
+++ backend/employee/views.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from .models import *
 from .serializer import *
+from collections import defaultdict
 
 # Create your views here.
 from django.http import HttpResponse
@@ -13,24 +14,55 @@ from django.http import HttpResponse
 
 
 class EmployeeView(APIView):
-    i = 0
+    # i = 0
+    
+    # Query employees
     def get(self, request):
-        EmployeeView.i += 1
-        print("Is asking" + str(EmployeeView.i))
-        employees = Employee.objects.all()
-        serializer = EmployeeSerializer(employees, many=True)
+        # EmployeeView.i += 1
+        # print("Is asking" + str(EmployeeView.i))
+        salespersons = Salesperson.objects.all()
+        #salespersons_serializer = SalespersonSerializer(salespersons, many=True)
+        purchasepersons = PurchasePerson.objects.all()
+        #purchasepersons_serializer = PurchasePersonSerializer(purchasepersons, many=True)
+        managers = Manager.objects.all()
+        #managers_serializer = ManagerSerializer(managers, many=True)
+
+        employees = salespersons + purchasepersons + managers
         
-        emp_ids = []
-        for employee in serializer.data:
-            emp_ids.append(employee['employee_id'])
+        saleperson_manager_relations = SalespersonManagerManagement.objects.all()
+        purchaseperson_manager_relations = PurchasepersonManagerManagement.objects.all()
+
+        managers_to_salepersons = defaultdict(list)
+        salepersons_to_managers = defaultdict(list)
+        managers_to_purchasepersons = defaultdict(list)
+        purchasepersons_to_managers = defaultdict(list)
+
+        for rel in saleperson_manager_relations:
+            managers_to_salepersons[rel.manager_id].append(rel.salesperson_id)
+            salepersons_to_managers[rel.salesperson_id].append(rel.manager_id)
         
+        for rel in purchaseperson_manager_relations:
+            managers_to_purchasepersons[rel.manager_id].append(rel.purchaseperson_id)
+            purchasepersons_to_managers[rel.purchaseperson_id].append(rel.manager_id)
+
+        for emp in employees:
+            if isinstance(emp, Salesperson):
+                emp.management = salepersons_to_managers.get(emp.employee_id, [])
+            elif isinstance(emp, PurchasePerson):
+                emp.management = purchasepersons_to_managers.get(emp.employee_id, [])
+            else:
+                emp.management = managers_to_salepersons.get(emp.employee_id, [])
+                emp.management += managers_to_purchasepersons.get(emp.employee_id, [])
+        
+        serializer = WholeEmployeeSerializer(employees, many=True)
+
         return Response(serializer.data)
     
     def post(self, request):
         print("POST received with data:", request.data)
-        serializer = EmployeeSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({"message": "Employee added successfully"}, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        # serializer = EmployeeSerializer(data=request.data)
+        # if serializer.is_valid():
+        #     serializer.save()
+        #     return Response({"message": "Employee added successfully"}, status=status.HTTP_201_CREATED)
+        # else:
+        #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
