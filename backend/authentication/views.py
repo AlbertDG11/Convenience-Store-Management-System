cf0cfbc5714aabbee92df16cf3b680f0cbc8326c
Fei Ding <dingfei.sgad@gmail.com>
2025-04-16 00:11:25 -0600

    Complete Preliminary Authentication Part

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from ..employee.model import *
+from rest_framework import status
+from rest_framework.response import Response
 
 # Create your views here.
+class AuthenticationView(APIView):
+    def post(self, request):
+        user_id = request.data.get('user_id')
+        password = request.data.get('password')
+
+        employee = Employee.objects.get(pk=user_id)
+        if employee and employee['password'] == password:
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_403_FORBIDDEN)
