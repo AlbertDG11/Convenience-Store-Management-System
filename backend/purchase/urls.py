6d7ee7c17d0b5c1f107bb4b9fac031f1f079c90b
Zhuojun Dong <albertdong11@gmail.com>
2025-04-20 20:59:09 -0600

    update urls

--- backend/purchase/urls.py
+++ backend/purchase/urls.py
@@ -3,6 +3,6 @@ from rest_framework.routers import DefaultRouter
 from backend.purchase.views import InventoryPurchaseViewSet
 
 router = DefaultRouter()
-router.register(r'inventory_purchases', InventoryPurchaseViewSet)
+router.register(r'purchases', InventoryPurchaseViewSet)
 
 urlpatterns = router.urls
