e6ed77bbe9b135d44f157dbf406b2d082f50f294
Fei Ding <dingfei.sgad@gmail.com>
2025-04-23 23:36:12 -0600

    Update some files

--- convenience_store/settings.py
+++ convenience_store/settings.py
@@ -105,16 +105,6 @@ WSGI_APPLICATION = 'convenience_store.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -162,16 +152,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'frontend/build/static',
-]
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            BASE_DIR / 'frontend/build',
-        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
